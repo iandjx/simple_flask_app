1bd151337e35d94ff54036219db8340d9e67b74b
Ian de Jesus <ian.dejesus.md@gmail.com>
2017-08-23 16:07:20 +0800

    41 Create application end points

--- app.py
+++ app.py
@@ -2,9 +2,51 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# POST - used to receive data
+# GET - used to send data back only
 
-@app.route('/')  # homepage
-def home():
-    return "Hello World"
+stores = [
+    {
+        'name': 'My Store',
+        'item': [
+            {
+                'name': 'shirt',
+                'price': 16.00
+            }
+        ]
+    }
+]
+
+
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+
+@app.route('/store')
+def get_stores():
+    pass
+
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    pass
+
+
+###
+
+# @app.route('/')
+# def home():
+#     return "Hello World"
 
 app.run(port=5000)
